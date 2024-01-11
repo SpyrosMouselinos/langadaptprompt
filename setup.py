1f5c97c97ce048a17479dca4f9a84008ca520106
Spyros Mouselinos <mouselinos.spur.kw@gmail.com>
2024-01-11 13:26:50 +0200

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='langadaptprompt',
+    version='2.2',
+    author='Spyridon Mouselinos',
+    author_email='mouselinos.spur.kw@gmail.com',
+    packages=find_packages(exclude='test'),
+    description='Port of LangChain prompting into pure Python functions.',
+    url='https://github.com/SpyrosMouselinos/langadaptprompt',
+    install_requires=[
+        'matplotlib',
+        'openai==0.28',
+        'pandas',
+        'transformers',
+        'sentence_transformers',
+        'torch',
+        'scipy',
+        'python-Levenshtein',
+        'Levenshtein',
+
+
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
